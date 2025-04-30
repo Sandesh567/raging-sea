37eba513f486f2a8753b570782b829d9b9f899d1
sandesh <sandeshbhatta218@gmail.com>
2025-04-30 16:59:00 +0545

    Importing three and creating debug, canvas and scene.

---
+++ src/script.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI({ width: 340 })
+const debugObject = {}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
