fb7f19eb7d3c498a237ed6e2922f85901049c0f8
sandesh <sandeshbhatta218@gmail.com>
2025-05-04 12:46:17 +0545

    Adding small frequency waves.

--- src/script.js
+++ src/script.js
@@ -64,6 +64,12 @@ gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).ste
 gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
 
 
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallIterations, 'value').min(0).max(5).step(1).name('uSmallIterations')
+
+
 /**
  * Sizes
  */
